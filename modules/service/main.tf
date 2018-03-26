b57b6dc663265b3f3c1220f221fcbf84c5c26f47
Rafi Kurnia Putra <rafi.putra@traveloka.com>
2018-03-26 10:13:46 +0700

    add role_force_detach_policies = true

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -4,7 +4,7 @@ data "aws_iam_policy_document" "this" {
 
     principals = {
       type        = "Service"
-      identifiers = "${var.aws_service}"
+      identifiers = ["${var.aws_service}"]
     }
   }
 }
@@ -12,8 +12,10 @@ data "aws_iam_policy_document" "this" {
 module "this" {
   source = "../../"
 
-  role_name          = "ServiceRoleFor${title(element(split(".", var.aws_service), 0))}_${join("-", split(" ", lower(var.role_purpose)))}"
-  role_path          = "/service-role/${var.aws_service}/"
-  role_description   = "${var.role_description}"
-  role_assume_policy = "${data.aws_iam_policy_document.this.json}"
+  role_name        = "ServiceRoleFor${title(element(split(".", var.aws_service), 0))}_${join("-", split(" ", lower(var.role_purpose)))}"
+  role_path        = "/service-role/${var.aws_service}/"
+  role_description = "${var.role_description}"
+
+  role_assume_policy         = "${data.aws_iam_policy_document.this.json}"
+  role_force_detach_policies = true
 }
