de8cb0bb061f455cb160a430a4ccae8ea77c2bbc
Rafi Kurnia Putra <rafi.putra@traveloka.com>
2018-03-26 08:54:27 +0700

    add lower interpolation

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "this" {
 module "this" {
   source = "../../"
 
-  role_name        = "ServiceRoleFor${title(element(split(".", var.aws_service), 0))_${join("-", split(" ", var.role_purpose))}}"
+  role_name        = "ServiceRoleFor${title(element(split(".", var.aws_service), 0))_${join("-", split(" ", lower(var.role_purpose)))}}"
   role_path        = "/service-role/${var.aws_service}/"
   role_description = "${var.role_description}"
   policy_document  = "${data.aws_iam_policy_document.services.json}"
