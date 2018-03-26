990ec28a7f18dc72bd8bc5b8ba0e720625b44983
Rafi Kurnia Putra <rafi.putra@traveloka.com>
2018-03-26 09:45:36 +0700

    add terraform version constraint

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+# This module was updated using terraform 0.11.4 at 2018/03/26.
+terraform {
+  required_version = ">= 0.11.4"
+}
+
 # Get the access to the effective Account ID, User ID, and ARN in which Terraform is authorized.
 data "aws_caller_identity" "current" {}
 
