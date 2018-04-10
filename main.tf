7df9d084017d635234b8352ec23079bb8cd47b82
Rafi Kurnia Putra <rafi.putra@traveloka.com>
2018-04-10 19:35:04 +0700

    added max_session_duration to the root module

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 # This module was updated using terraform 0.11.4 at 2018/03/26.
 terraform {
-  required_version = ">= 0.11.4"
+  required_version = "~> 0.11.4"
+}
+
+# Added provider AWS version constraint. `max_session_duration` is supported on 1.14.0
+provider "aws" {
+  version = "~> 1.14"
 }
 
 # Get the access to the effective Account ID, User ID, and ARN in which Terraform is authorized.
@@ -14,4 +19,5 @@ resource "aws_iam_role" "this" {
 
   assume_role_policy    = "${var.role_assume_policy}"
   force_detach_policies = "${var.role_force_detach_policies}"
+  max_session_duration  = "${var.role_max_session_duration}"
 }
--- variables.tf
+++ variables.tf
@@ -23,3 +23,8 @@ variable "role_force_detach_policies" {
   description = "Specifies to force detaching any policies the role has before destroying it."
   default     = false
 }
+
+variable "role_max_session_duration" {
+  description = "The maximum session duration (in seconds) that you want to set for the specified role. If you do not specify a value for this setting, the default maximum of one hour is applied. This setting can have a value from 1 hour to 12 hours."
+  default     = 3600
+}
