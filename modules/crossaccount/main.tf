e0ab9e30ce384ea584bf903fdf798749d7932e56
Nur Adhi Nugroho <nur.nugroho@traveloka.com>
2020-09-17 16:33:58 +0700

    Upgrad Terraform v0.12 Syntax
    Signed-off-by: Nur Adhi Nugroho <nur.nugroho@traveloka.com>

--- modules/crossaccount/main.tf
+++ modules/crossaccount/main.tf
@@ -1,41 +1,45 @@
 data "aws_iam_policy_document" "doc" {
-  statement = {
+  statement {
     actions = ["sts:AssumeRole"]
 
-    principals = {
+    principals {
       type        = "AWS"
-      identifiers = ["${var.trusted_role_arns}"]
+      identifiers = var.trusted_role_arns
     }
   }
 }
 
 module "aws-resource-naming_iam_role" {
-  source        = "git::https://github.com/traveloka/terraform-aws-resource-naming.git?ref=v0.13.0"
-  name_prefix   = "${var.role_name}"
+  source        = "git::https://github.com/traveloka/terraform-aws-resource-naming.git?ref=v0.19.0"
+  name_prefix   = var.role_name
   resource_type = "iam_role"
 }
 
 locals {
-  additional_role_path = "${substr(var.role_path, 0, 1) == "/" ? substr(var.role_path, 1, length(var.role_path)-1) : var.role_path}"
+  additional_role_path = substr(var.role_path, 0, 1) == "/" ? substr(var.role_path, 1, length(var.role_path) - 1) : var.role_path
 }
 
 # Module, the parent module.
 module "crossacount" {
   source = "../../"
 
-  role_name        = "${module.aws-resource-naming_iam_role.name}"
+  role_name        = module.aws-resource-naming_iam_role.name
   role_path        = "/crossaccount/${local.additional_role_path}"
-  role_description = "${var.role_description}"
+  role_description = var.role_description
 
-  role_tags = "${merge(var.role_tags, map(
-    "Service", "${var.service_name}"
-    ))}"
+  role_tags = merge(
+    var.role_tags,
+    {
+      "Service" = var.service_name
+    },
+  )
 
-  role_assume_policy         = "${data.aws_iam_policy_document.doc.json}"
-  role_permission_boundary   = "${var.role_permission_boundary}"
-  role_force_detach_policies = "${var.role_force_detach_policies}"
-  role_max_session_duration  = "${var.role_max_session_duration}"
+  role_assume_policy         = data.aws_iam_policy_document.doc.json
+  role_permission_boundary   = var.role_permission_boundary
+  role_force_detach_policies = var.role_force_detach_policies
+  role_max_session_duration  = var.role_max_session_duration
 
-  product_domain = "${var.product_domain}"
-  environment    = "${var.environment}"
+  product_domain = var.product_domain
+  environment    = var.environment
 }
+
--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,40 @@
 output "aws_account_id" {
   description = "The AWS Account ID number of the account that owns or contains the calling entity."
-  value       = "${data.aws_caller_identity.current.account_id}"
+  value       = data.aws_caller_identity.current.account_id
 }
 
 output "aws_caller_arn" {
   description = "The AWS ARN associated with the calling entity."
-  value       = "${data.aws_caller_identity.current.arn}"
+  value       = data.aws_caller_identity.current.arn
 }
 
 output "aws_caller_user_id" {
   description = "The unique identifier of the calling entity."
-  value       = "${data.aws_caller_identity.current.user_id}"
+  value       = data.aws_caller_identity.current.user_id
 }
 
 output "role_name" {
   description = "The name of the role."
-  value       = "${element(concat(aws_iam_role.this.*.name, list("")), 0)}"
+  value       = element(concat(aws_iam_role.this.*.name, [""]), 0)
 }
 
 output "role_arn" {
   description = "The Amazon Resource Name (ARN) specifying the role."
-  value       = "${element(concat(aws_iam_role.this.*.arn, list("")), 0)}"
+  value       = element(concat(aws_iam_role.this.*.arn, [""]), 0)
 }
 
 output "role_description" {
   description = "The description of the role."
-  value       = "${element(concat(aws_iam_role.this.*.description, list("")), 0)}"
+  value       = element(concat(aws_iam_role.this.*.description, [""]), 0)
 }
 
 output "role_create_date" {
   description = "The creation date of the IAM role."
-  value       = "${element(concat(aws_iam_role.this.*.create_date, list("")), 0)}"
+  value       = element(concat(aws_iam_role.this.*.create_date, [""]), 0)
 }
 
 output "role_unique_id" {
   description = "The stable and unique string identifying the role."
-  value       = "${element(concat(aws_iam_role.this.*.unique_id, list("")), 0)}"
+  value       = element(concat(aws_iam_role.this.*.unique_id, [""]), 0)
 }
+
