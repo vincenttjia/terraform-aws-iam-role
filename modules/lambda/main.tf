5d238aab1f4fa4c1cb334f481e6695b4faa95908
SiahaanBernard <SiahaanBernard@users.noreply.github.com>
2020-10-01 12:56:03 +0700

    Update resource naming module version to fix division bug problem. (#44)

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -5,7 +5,7 @@ locals {
 }
 
 module "random" {
-  source = "github.com/traveloka/terraform-aws-resource-naming.git?ref=v0.19.0"
+  source = "github.com/traveloka/terraform-aws-resource-naming.git?ref=v0.19.1"
 
   name_prefix   = local.name_prefix
   resource_type = "iam_role"
